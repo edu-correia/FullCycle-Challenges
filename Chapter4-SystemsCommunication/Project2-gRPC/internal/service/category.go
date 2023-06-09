ad9077a246b4a7d7f9b0a142e4b78e36961a79da
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2023-06-09 15:15:39 -0300

    feat: implementing createCategory from gRPC

---
+++ Chapter4-SystemsCommunication/Project2-gRPC/internal/service/category.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+	"io"
+
+	"github.com/edu-correia/grpc/internal/database"
+	"github.com/edu-correia/grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return categoryResponse, nil
+}
